86700ab4ddf280ecbb77b9532647801ac1b39dd1
Hunter Lang <hunter.lang@workday.com>
2019-02-01 14:26:04 -0700

    Add in cdup to quickly go up multiple directories.

--- util/util-plugin.zsh
+++ util/util-plugin.zsh
@@ -1,1 +1,11 @@
 alias myip="ifconfig en0 | grep inet | grep -v inet6 | awk '{printf \"%s\", \$2}'"
+
+function cdup {
+	if ! [[ $1 =~ '^[0-9]+$' && $1 > 0 ]]; then
+		echo "Entered value is invalid: $1"
+		return 1
+	fi
+	for i in $(seq 1 $1); do
+		cd ..
+	done
+}
