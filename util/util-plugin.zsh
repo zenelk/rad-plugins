60d75397a4ce26e817900cc897e3ce9fe1775e86
Hunter Lang <hunter.lang@workday.com>
2021-04-23 11:11:27 -0600

    Put back history hook for ignoring bad commands, but just putting it in util for now

--- util/util-plugin.zsh
+++ util/util-plugin.zsh
@@ -3,11 +3,27 @@ alias ls='ls -al'
 
 function_redefine cdup
 function cdup() {
-	if ! [[ $1 =~ '^[0-9]+$' && $1 > 0 ]]; then
-		echo "Entered value is invalid: $1"
-		return 1
-	fi
-	for i in $(seq 1 $1); do
-		cd ..
-	done
+    if ! [[ $1 =~ '^[0-9]+$' && $1 > 0 ]]; then
+        echo "Entered value is invalid: $1"
+        return 1
+    fi
+    for i in $(seq 1 $1); do
+        cd ..
+    done
+}
+
+# Hook for tying into ZSH process for history adding
+function zshaddhistory() {
+    emulate -L zsh
+
+    # Ignore failed commands
+    whence ${${(z)1}[1]} >| /dev/null || return 1
+
+    # Ignore commands that start with a single space
+    if [[ "$1" =~ "^ " ]]; then
+        return 1
+    fi
+
+    print -sr -- "$no_newline"
+    fc -p
 }
