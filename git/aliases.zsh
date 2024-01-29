5b286ada5da924b5c5d5af59f2a93ff2105d2fd9
Hunter Lang <hunter.lang@workday.com>
2024-01-29 13:31:19 -0700

    Add gu alias for git restore --staged

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -13,3 +13,4 @@ alias gc='git commit'
 alias ggg="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) \
 - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          \
 %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+alias gu='git restore --staged'
