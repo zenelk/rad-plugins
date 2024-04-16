1ccf69e1f74d2a2e8cd444f59e34244f2024e068
Hunter Lang <hunter.lang@workday.com>
2024-04-16 13:48:23 -0600

    Move blk, smp, pyt, and pyv to this repo

--- python/python-plugin.zsh
+++ python/python-plugin.zsh
@@ -1,1 +1,34 @@
-alias dpyt='pytest --cov-report term-missing --cov=src --tb=auto -l ""'
+alias blk='black .'
+alias smp='mypy --strict .'
+
+function_redefine pyt
+function pyt() {
+  pytest "$@" --cov-report term-missing --cov=src --tb=auto -l -rPx
+}
+
+function_redefine pyv
+function pyv() {
+  echo 'Running Black...'
+  blk
+  if [ $? -ne 0 ]; then
+    echo -e "blk command failed!\n" >&2
+  else
+    echo -e 'Black succeeded!\n'
+  fi
+
+  echo 'Running Mypy...'
+  smp
+  if [ $? -ne 0 ]; then
+    echo -e "smp command failed!\n" >&2
+  else
+    echo -e 'Mypy succeeded!\n'
+  fi
+
+  echo 'Running Pytest...'
+  pyt
+  if [ $? -ne 0 ]; then
+    echo "pyt command failed!" >&2
+  else
+    echo 'Pytest succeeded!'
+  fi
+}
