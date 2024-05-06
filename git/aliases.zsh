e05b5dcbc0a72bdbdb35d875804f57577672b059
Hunter Lang <hunter.lang@workday.com>
2024-05-06 13:15:50 -0600

    Add aliases for 'git commit --amend --no-edit' and 'git clone'

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -14,3 +14,5 @@ alias ggg="git log --graph --abbrev-commit --decorate --format=format:'%C(bold b
 - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          \
 %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
 alias gu='git restore --staged'
+alias gcan='git commit --amend --no-edit'
+alias gcl='git clone'
