8835ab19f52207d849e33dec6678001369adef41
Hunter Lang <hunter.lang@workday.com>
2024-05-14 19:36:12 -0600

    Reworked history adding to not add commands that don't exist, commands that start with any space, or commands in the ignored list

--- init/init-plugin.zsh
+++ init/init-plugin.zsh
@@ -18,16 +18,44 @@ function function_redefine() {
 # Hook for tying into ZSH process for history adding
 function_redefine zshaddhistory
 function zshaddhistory() {
+  function read_ignored_commands() {
+    local result=()
+    while IFS= read -r line; do
+      if [[ "${line}" =~ '^#' ]] || [ -z "${line}" ]; then
+        continue
+      fi
+
+      result+=("${line}")
+    done < "${INIT_LOAD_PATH}/ignored_commands"
+
+    echo "${result[@]}"
+  }
+
+  function is_ignored_command() {
+    local ignored_commands=($(read_ignored_commands))
+    local ignored_regex="^($(echo "${ignored_commands[@]}" | tr ' ' '|'))"
+    [[ "${1}" =~ $ignored_regex ]]
+  }
+
   emulate -L zsh
 
-  # Ignore failed commands
-  whence ${${(z)1}[1]} >| /dev/null || return 1
+  if ! whence ${${(z)1}[1]} >| /dev/null; then
+    echo "[ZK] Command does not exist on the system. Not adding to history."
+    return 1
+  fi
 
-  # Ignore commands that start with a single space
-  if [[ "$1" =~ "^ " ]]; then
+  if [[ "${1}" =~ "^[[:space:]]+" ]]; then
+    echo "[ZK] Command starts with whitespace. Not adding to history."
+    return 1
+  fi
+
+  if is_ignored_command "${1}"; then
+    echo "[ZK] Command is in ignored list. Not adding to history."
     return 1
   fi
 
   print -sr -- "${1%%$'\n'}"
   fc -p
 }
+
+export INIT_LOAD_PATH="${0:a:h}"
