549c20c8dc9883e5880b46d1a030b6f96b959c70
Hunter Lang <hunter.lang@workday.com>
2024-05-15 18:19:19 -0600

    Add new function gbcl to clean up repos and update branches

--- git/one-offs.zsh
+++ git/one-offs.zsh
@@ -22,10 +22,10 @@ function gbnr() {
   fi
 
   local branches="$(git branch -vv | cut -c 3- | grep ': gone]' | awk '{print $1}')"
-  
+
   if [[ -z "${branches}" ]]; then
     echo "No branches found locally that are removed from the remote."
-    return 0
+    return 1
   else
     echo "${branches}"
   fi
@@ -33,4 +33,37 @@ function gbnr() {
   if [[ "${1}" = '-d' ]]; then
     echo "${branches}" | xargs git branch -D
   fi
+  return 0
+}
+
+function_redefine gbcl
+function gbcl() {
+  echo "Fetching..."
+  gf
+
+  echo "Pulling from origin..."
+  gl
+
+  if [ "$(git remote | wc -l)" -gt 1 ]; then
+    echo "Found more than one remote, also pulling in upstream..."
+    gl upstream "$(gb)"
+  fi
+
+  echo "Pushing current branch..."
+  gp
+
+  echo "Starting gone branch cleanup..."
+  gbnr
+  exit_code=$?
+  if [ $exit_code -eq 0 ]; then
+    echo "Do you want to remove these branches? (y/n)"
+    read -r response
+    if [ "${response,,}" = "y" ]; then
+      gbnr -d
+    else
+      echo "Branches not removed."
+    fi
+  fi
+
+  echo "Done."
 }
