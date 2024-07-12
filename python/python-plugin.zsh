3efa42796ffb674e8ce37d98d8e938d61fb423c3
Hunter Lang <hunter.lang@workday.com>
2024-07-12 17:48:38 -0600

    Rework blk, smp, and pyt into the pyv function through parameters

--- python/python-plugin.zsh
+++ python/python-plugin.zsh
@@ -1,34 +1,76 @@
-alias blk='black .'
-alias smp='mypy --exclude build --strict .'
-
-function_redefine pyt
-function pyt() {
-  pytest "$@" --cov-report term-missing --cov=src --tb=auto -l -rPx
-}
-
 function_redefine pyv
 function pyv() {
-  echo 'Running Black...'
-  blk
-  if [ $? -ne 0 ]; then
-    echo -e "blk command failed!\n" >&2
-  else
-    echo -e 'Black succeeded!\n'
+  do_black=false
+  do_mypy=false
+  do_pytest=false
+  should_shift=false
+
+  while getopts "bmp" opt; do
+    case $opt in
+      b)
+        echo "Option -b: $OPTARG"
+        do_black=true
+        should_shift=true
+        ;;
+      m)
+        echo "Option -m: $OPTARG"
+        do_mypy=true
+        should_shift=true
+        ;;
+      p)
+        echo "Option -p: $OPTARG"
+        do_pytest=true
+        should_shift=true
+        ;;
+      \?)
+        echo "Invalid option: -$OPTARG" >&2
+        return 1
+        ;;
+    esac
+  done
+
+  if [ "$should_shift" = true ]; then
+    shift $((OPTIND - 1))
   fi
 
-  echo 'Running Mypy...'
-  smp
-  if [ $? -ne 0 ]; then
-    echo -e "smp command failed!\n" >&2
-  else
-    echo -e 'Mypy succeeded!\n'
+  if [ -z "$1" ]; then
+    echo "Usage: pyv [-b] [-m] [-p] PATH..." >&2
+    return 1
   fi
 
-  echo 'Running Pytest...'
-  pyt
-  if [ $? -ne 0 ]; then
-    echo "pyt command failed!" >&2
-  else
-    echo 'Pytest succeeded!'
+  if [ "$do_black" = false ] && [ "$do_mypy" = false ] && [ "$do_pytest" = false ]; then
+    do_black=true
+    do_mypy=true
+    do_pytest=true
+  fi
+
+  if [ "$do_black" = true ]; then
+    echo "Running Black with args '$@'..."
+    black "$@"
+    if [ $? -ne 0 ]; then
+      echo -e "blk command failed!\n" >&2
+    else
+      echo -e 'Black succeeded!\n'
+    fi
+  fi
+
+  if [ "$do_mypy" = true ]; then
+    echo "Running Mypy with args '$@'..."
+    mypy --exclude build --strict "$@"
+    if [ $? -ne 0 ]; then
+      echo -e "smp command failed!\n" >&2
+    else
+      echo -e 'Mypy succeeded!\n'
+    fi
+  fi
+
+  if [ "$do_pytest" = true ]; then
+    echo "Running Pytest with args '$@'..."
+    pytest "$@" --cov-report term-missing --cov=src --tb=auto -l -rPx
+    if [ $? -ne 0 ]; then
+      echo -e "pyt command failed!\n" >&2
+    else
+      echo -e 'Pytest succeeded!\n'
+    fi
   fi
 }
