c65c8eb3b27ced78914bab24fac7183a0a724fc8
Hunter Lang <hunter.lang@workday.com>
2024-12-11 16:26:50 -0700

    Create functions for managing Python virtual environments

--- python/python-plugin.zsh
+++ python/python-plugin.zsh
@@ -1,3 +1,202 @@
+ZK_VENV_ROOT="${HOME}/.venv"
+
+function_redefine venv
+function venv() {
+  function _venv_usage() {
+    echo "Usage: venv [verb] [options]"
+    echo "  [verb] (default: list) is one of:"
+    echo "    activate   (a)"
+    echo "    create     (c)"
+    echo "    deactivate (da)"
+    echo "    destroy    (d)"
+    echo "    help       (h)"
+    echo "    list       (l)"
+    echo "    recreate   (r)"
+    echo "  [options] are arguments defined in the respective verbs"
+  }
+
+  if [ -z "${1}" ]; then
+    _venv_usage
+    return 1
+  fi
+
+  local verb="${1}"
+  shift
+
+  case "${verb}" in
+    'activate' | 'a')
+      _venv_activate "${@}"
+      ;;
+    'create' | 'c')
+      _venv_create "${@}"
+      ;;
+    'destroy' | 'd')
+      _venv_destroy "${@}"
+      ;;
+    'deactivate' | 'da')
+      _venv_deactivate "${@}"
+      ;;
+    '' | 'help' | 'h')
+      _venv_usage
+      ;;
+    'list' | 'l')
+      _venv_list "${@}"
+      ;;
+    'recreate' | 'r')
+      _venv_recreate "${@}"
+      ;;
+    *)
+      echo "Invalid verb: '${verb}'!"
+      _venv_usage
+      return 1
+      ;;
+  esac
+}
+
+function_redefine _inside_venv
+function _inside_venv() {
+  if [[ "$(command -v python)" =~ ${ZK_VENV_ROOT}/.* ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function_redefine _current_venv_name
+function _current_venv_name() {
+  if _inside_venv; then
+    command -v python | sed "s|${ZK_VENV_ROOT}/||" | cut -d '/' -f2
+  else
+    echo ""
+  fi
+}
+
+function_redefine _python_version
+function _python_version() {
+  python --version | cut -d ' ' -f 2
+}
+
+function_redefine _venv_deactivate_if_inside
+function _venv_deactivate_if_inside() {
+  if _inside_venv; then
+    local current_venv_name="$(_current_venv_name)"
+    echo "Already inside Python virtual environment '${current_venv_name}'. Deactivating..."
+    _venv_deactivate
+  fi
+}
+
+function_redefine _venv_activate
+function _venv_activate() {
+  function _venv_activate_usage() {
+    echo "Usage: venv activate VENV_NAME"
+  }
+
+  _venv_deactivate_if_inside
+
+  local target_venv_name="${1}"
+  if [ -z "${target_venv_name}" ]; then
+    echo "Missing target virtual environment name!"
+    _venv_activate_usage
+    return 1
+  fi
+
+  echo "Activating Python virtual environment '${target_venv_name}'..."
+
+  python_version="$(_python_version)"
+  if ! source "${ZK_VENV_ROOT}/${python_version}/${target_venv_name}/bin/activate"; then
+    echo "Failed to activate Python virtual environment '${target_venv_name}'!"
+    return 2
+  fi
+}
+
+function_redefine _venv_create
+function _venv_create() {
+  function _venv_create_usage() {
+    echo "Usage: venv create VENV_NAME"
+  }
+
+  local target_venv_name="${1}"
+  if [ -z "${target_venv_name}" ]; then
+    echo "Missing target virtual environment name!"
+    _venv_create_usage
+    return 1
+  fi
+
+  local python_version="$(_python_version)"
+  local venv_path="${ZK_VENV_ROOT}/${python_version}/${target_venv_name}"
+  if [ -d "${venv_path}" ]; then
+    echo "Python virtual environment '${target_venv_name}' already exists!"
+    return 2
+  fi
+
+  echo "Creating Python virtual environment '${target_venv_name}'..."
+  if ! python -m venv "${venv_path}"; then
+    echo "Failed to create Python virtual environment '${target_venv_name}'!"
+    return 3
+  fi
+}
+
+function_redefine _venv_deactivate
+function _venv_deactivate() {
+  local current_venv_name="$(_current_venv_name)"
+  echo "Deactivating Python virtual environment '${current_venv_name}'..."
+  if ! deactivate; then
+    echo "Failed to deactivate Python virtual environment '${current_venv_name}'!"
+    return 1
+  fi
+}
+
+function_redefine _venv_destroy
+function _venv_destroy() {
+  function _venv_destroy_usage() {
+    echo "Usage: venv destroy VENV_NAME"
+  }
+
+  local target_venv_name="${1}"
+  if [ -z "${target_venv_name}" ]; then
+    echo "Missing target virtual environment name!"
+    _venv_destroy_usage
+    return 1
+  fi
+
+  local python_version="$(_python_version)"
+  local venv_path="${ZK_VENV_ROOT}/${python_version}/${target_venv_name}"
+
+  echo "Destroying Python virtual environment '${target_venv_name}'..."
+  if ! rm -rf "${venv_path}"; then
+    echo "Failed to destroy Python virtual environment '${target_venv_name}'!"
+    return 3
+  fi
+}
+
+function_redefine _venv_list
+function _venv_list() {
+  local python_version="$(_python_version)"
+  local venvs_path="${ZK_VENV_ROOT}/${python_version}"
+
+  echo "Python virtual environments for Python version ${python_version}:"
+  for venv in $(ls -1 "${venvs_path}" | grep -v -e '^\.$' -e '^\.\.$'); do
+    echo "  ${venv}"
+  done
+}
+
+function_redefine _venv_recreate
+function _venv_recreate() {
+  function _venv_recreate_usage() {
+    echo "Usage: venv recreate VENV_NAME"
+  }
+
+  local target_venv_name="${1}"
+  if [ -z "${target_venv_name}" ]; then
+    echo "Missing target virtual environment name!"
+    _venv_recreate_usage
+    return 1
+  fi
+
+  _venv_destroy "${target_venv_name}"
+  _venv_create "${target_venv_name}"
+}
+
 function_redefine pyv
 function pyv() {
   do_black=false
